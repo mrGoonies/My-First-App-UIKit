beeab6b5bee60125bfc3795da42bce90d7f5e399
mrGoonies <gooniesdev@gmail.com>
2022-06-14 11:09:29 -0400

    adding functionality

--- My-First-App/ViewController.swift
+++ My-First-App/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textOnScreen: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textOnScreen.textAlignment = .center
+        
     }
-
+    
+    
+    @IBAction func changeTheText(_ sender: Any) {
+        let randomText: Set = [
+            "Hello World",
+            "Hola Món",
+            "你好世界",
+            "Salut monde",
+            "Hallo Welt",
+            "Ciao mondo",
+            "こんにちは世界",
+            "Witaj świecie",
+            "Olá mundo"
+        ]
+        
+        textOnScreen.text = randomText.randomElement()
+    }
+    
 
 }
 
